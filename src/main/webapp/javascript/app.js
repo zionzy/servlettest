5602587aaae4df9092768028a7a4b024ddeb9a24
Zion Yuan <zyuan@ziji-media.com>
2015-03-30 17:24:22 -0400

    more update to angular, add ng-click.

--- src/main/webapp/javascript/app.js
+++ src/main/webapp/javascript/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', []);
+(function(){
+	var myApp = angular.module('myAppModule', []);
+	
+	myApp.controller('myController', ['$scope', function($scope){
+		
+		$scope.changeParagraph = function(test) {
+			
+			document.getElementById("demo").innerHTML = "Paragraph changed.";
+			
+			$scope.spice = test;
+		};
+	}]);
+})();
+
